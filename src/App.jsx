9b90762c5057b515f300feab3a1cfbc706b78ae7
Abdulfetah Jemal <abdulfetahjemal7se@gmail.com>
2024-03-15 10:07:11 +0300

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import React from "react";
 function App() {
   return (
     <div className="flex justify-center items-center h-[100vh] w-full flex-col">
-     <h1 className="m-2 text-[2rem]">The Wadeh Blog page ....</h1>
-     <h1 className="m-2 text-[1rem]">Coming Soon !</h1>
-     <button className="px-2 py-1 bg-green-400 rounded-lg ">Click me</button>
+      <h1 className="m-2 text-[2rem]">The Wadeh Blog page ....</h1>
+      <h1 className="m-2 text-[1.25rem]">Coming Soon !</h1>
+      <button className="px-2 py-1 bg-green-500 rounded-lg font-bold text-white ">
+        Click me
+      </button>
     </div>
   );
 }
